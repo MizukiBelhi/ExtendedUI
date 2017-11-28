fbb1d8bc6315c051e5a8ae78a41d6a30e24c54d2
MizukiBelhi <poipoifumi@web.de>
2017-11-28 04:00:35 -0800

    appveyor

--- build/build.ps1
+++ build/build.ps1
@@ -10,11 +10,11 @@ Get-ChildItem -Filter . .\src\*.lua | Copy-Item -Destination $ipfDir
 Get-ChildItem -Filter . .\src\*.xml | Copy-Item -Destination $ipfDir
 
 Write-Output "Creating IPF file"
-py -3 ..\build\buildtools\ipf.py -c -f "$outputDir\$name.ipf" "$outputDir\temp"
+py -3 $PSScriptRoot\buildtools\ipf.py -c -f "$outputDir\$name.ipf" "$outputDir\temp"
 Write-Output "-done"
 
 Write-Output "Encrypting IPF file"
-& ..\build\buildtools\IPFUnpacker\ipf_unpack.exe "$outputDir\$name.ipf" encrypt
+& $PSScriptRoot\buildtools\IPFUnpacker\ipf_unpack.exe "$outputDir\$name.ipf" encrypt
 Move-Item "$outputDir\$name.ipf" "$outputDir\$name.ipf"
 
 Write-Output "Done"
