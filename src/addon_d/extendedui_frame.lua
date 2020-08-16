5ce20ae4e2eb06f1f299d4950b57d44cd4675c38
Mizuki <MizukiBelhi@users.noreply.github.com>
2020-08-15 18:58:19 -0700

    Update extendedui_frame.lua

--- src/addon_d/extendedui_frame.lua
+++ src/addon_d/extendedui_frame.lua
@@ -315,6 +315,8 @@ function EXTENDEDUI_ON_FRAME_LOADS()
 	euiFrame = extui.AddFrame("minimized_tp_button", "TP Shop Button");
 	euiFrame.saveHidden = true;
 	euiFrame = extui.AddFrame("minimized_godprotection_button", "God Protect Button");
+	euiFrame = extui.AddFrame("minimized_housing_promote_board", "Personal Housing");
+    euiFrame = extui.AddFrame("minimized_guild_housing", "Guild Housing");
 	
 	
 	-- Fix by Sadlion, modified into a ternary by me	
